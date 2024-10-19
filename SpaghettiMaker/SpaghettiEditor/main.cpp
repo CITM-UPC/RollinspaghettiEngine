85de29104aa5a2c7b45f6700ae57509e621292ed
PabloL2003 <125266074+PabloL2003@users.noreply.github.com>
2024-10-19 16:08:15 +0200

    Starting point

---
+++ SpaghettiMaker/SpaghettiEditor/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "..\SpaghettiEngine/GameObject.h"
+
+using namespace std;
+
+int main()
+{
+	GameObject go;
+	go.paint();
+	return 0;
+}
--- SpaghettiMaker/SpaghettiEngine/GameObject.h
+++ SpaghettiMaker/SpaghettiEngine/GameObject.h
@@ -1,5 +1,8 @@
 #pragma once
 class GameObject
 {
+public:
+	void paint();
+
 };
 
