fc922108a123bc37acd8afc230846026855f8d98
PabloL2003 <125266074+PabloL2003@users.noreply.github.com>
2024-10-19 19:37:25 +0200

    Test of rendering window ( needs to be separated to a different class)

--- SpaghettiMaker/SpaghettiEditor/main.cpp
+++ SpaghettiMaker/SpaghettiEditor/main.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
 #include "SpaghettiEngine/GameObject.h"
 #include "imgui.h"
+#include "SDL2/SDL_video.h"
 
 using namespace std;
 
 int main()
 {
+	SDL_Window* _window = nullptr;
+	void* _ctx = nullptr;
+
+	unsigned short _width = 800;
+	unsigned short _height = 600;
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    _window = SDL_CreateWindow("NYUNITY", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _width, _height, SDL_WINDOW_OPENGL);
+    if (!_window) throw exception(SDL_GetError());
+
+    _ctx = SDL_GL_CreateContext(_window);
+    if (!_ctx) throw exception(SDL_GetError());
+    if (SDL_GL_MakeCurrent(_window, _ctx) != 0) throw exception(SDL_GetError());
+    if (SDL_GL_SetSwapInterval(1) != 0) throw exception(SDL_GetError());
+
+
 	GameObject go;
 	go.paint();
+	while (true)
+	{
+		
+	}
 	return 0;
 }
