377ccd9b72d09570eb6bf20b3e26b31b66c26617
Yeeway69 <yeyiwei1@gmail.com>
2024-10-28 22:27:57 +0100

    added texture base class but namespace issues to solve
    Realized the Include folder for DevIl is now IL

---
+++ SpaghettiMaker/SpaghettiEngine/Texture.cpp
@@ -0,0 +1,143 @@
+#include "Texture.h"
+#include <GL/glew.h>
+#include "IL/il.h"
+#include "IL/ilu.h"
+#include <vector>
+#include <iostream>
+
+bool Texture::LoadFromFile(const std::string& path) {
+    Cleanup();
+    _path = path;
+
+    // Generate DevIL image ID
+    ILuint imageID;
+    ilGenImages(1, &imageID);
+    ilBindImage(imageID);
+
+    // Load image
+    if (!ilLoadImage(path.c_str())) {
+        std::cerr << "Failed to load texture: " << path << std::endl;
+        ilDeleteImages(1, &imageID);
+        return false;
+    }
+
+    // Convert image to RGBA format
+    if (!ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE)) {
+        std::cerr << "Failed to convert image: " << path << std::endl;
+        ilDeleteImages(1, &imageID);
+        return false;
+    }
+
+    _width = ilGetInteger(IL_IMAGE_WIDTH);
+    _height = ilGetInteger(IL_IMAGE_HEIGHT);
+    _channels = 4; // RGBA
+
+    // Create OpenGL texture
+    glGenTextures(1, &_textureID);
+    glBindTexture(GL_TEXTURE_2D, _textureID);
+
+    // Set default parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, _minFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, _magFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, _wrapS);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, _wrapT);
+
+    // Upload texture data
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, ilGetData());
+    GenerateMipmaps();
+
+    // Cleanup DevIL image
+    ilDeleteImages(1, &imageID);
+
+    _isLoaded = true;
+    return true;
+}
+
+bool Texture::LoadFromMemory(unsigned char* data, int width, int height, int channels) {
+    Cleanup();
+
+    _width = width;
+    _height = height;
+    _channels = channels;
+
+    glGenTextures(1, &_textureID);
+    glBindTexture(GL_TEXTURE_2D, _textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, _minFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, _magFilter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, _wrapS);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, _wrapT);
+
+    GLenum format = (channels == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    GenerateMipmaps();
+
+    _isLoaded = true;
+    return true;
+}
+
+std::shared_ptr<Texture> Texture::CreateCheckerboard(int width, int height) {
+    auto texture = std::make_shared<Texture>();
+    std::vector<unsigned char> data(width * height * 4);
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            bool isWhite = ((x / 8) + (y / 8)) % 2 == 0;
+            unsigned char color = isWhite ? 255 : 128;
+
+            data[(y * width + x) * 4 + 0] = color;  // R
+            data[(y * width + x) * 4 + 1] = color;  // G
+            data[(y * width + x) * 4 + 2] = color;  // B
+            data[(y * width + x) * 4 + 3] = 255;    // A
+        }
+    }
+
+    texture->LoadFromMemory(data.data(), width, height, 4);
+    return texture;
+}
+
+void Texture::Bind(unsigned int slot) const {
+    glActiveTexture(GL_TEXTURE0 + slot);
+    glBindTexture(GL_TEXTURE_2D, _textureID);
+}
+
+void Texture::Unbind() const {
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::Cleanup() {
+    if (_textureID) {
+        glDeleteTextures(1, &_textureID);
+        _textureID = 0;
+    }
+    _isLoaded = false;
+}
+
+void Texture::SetFiltering(GLint minFilter, GLint magFilter) {
+    _minFilter = minFilter;
+    _magFilter = magFilter;
+
+    if (_textureID) {
+        glBindTexture(GL_TEXTURE_2D, _textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
+    }
+}
+
+void Texture::SetWrapping(GLint wrapS, GLint wrapT) {
+    _wrapS = wrapS;
+    _wrapT = wrapT;
+
+    if (_textureID) {
+        glBindTexture(GL_TEXTURE_2D, _textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
+    }
+}
+
+void Texture::GenerateMipmaps() {
+    if (_textureID) {
+        glBindTexture(GL_TEXTURE_2D, _textureID);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+}
---
+++ SpaghettiMaker/SpaghettiEngine/Texture.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <string>
+#include <GL/glew.h>
+#include <memory>
+
+class Texture {
+private:
+    unsigned int _textureID = 0;
+    int _width = 0;
+    int _height = 0;
+    int _channels = 0;
+    std::string _path;
+    bool _isLoaded = false;
+
+    // Texture parameters
+    GLint _minFilter = GL_LINEAR_MIPMAP_LINEAR;
+    GLint _magFilter = GL_LINEAR;
+    GLint _wrapS = GL_REPEAT;
+    GLint _wrapT = GL_REPEAT;
+
+public:
+    Texture() = default;
+    ~Texture() { Cleanup(); }
+
+    bool LoadFromFile(const std::string& path);
+    bool LoadFromMemory(unsigned char* data, int width, int height, int channels);
+    static std::shared_ptr<Texture> CreateCheckerboard(int width = 64, int height = 64);
+
+    void Bind(unsigned int slot = 0) const;
+    void Unbind() const;
+    void Cleanup();
+
+    // Texture parameter setters
+    void SetFiltering(GLint minFilter, GLint magFilter);
+    void SetWrapping(GLint wrapS, GLint wrapT);
+    void GenerateMipmaps();
+
+    // Getters
+    unsigned int GetID() const { return _textureID; }
+    int GetWidth() const { return _width; }
+    int GetHeight() const { return _height; }
+    const std::string& GetPath() const { return _path; }
+    bool IsLoaded() const { return _isLoaded; }
+};
+
+using TexturePtr = std::shared_ptr<Texture>;
