93dc9c4db4d212ea498a2891c9529ee4bfa59fb3
Yeeway69 <yeyiwei1@gmail.com>
2024-10-29 16:35:54 +0100

    Comment unused virtual methods

--- SpaghettiMaker/SpaghettiEngine/PrimitiveMenu.h
+++ SpaghettiMaker/SpaghettiEngine/PrimitiveMenu.h
@@ -18,18 +18,18 @@ public:
                 GameObject* sphere = PrimitiveGenerator::CreateSphere();
                 activeScene->CreateGameObject(sphere->GetName().c_str(), sphere);
             }
-            if (ImGui::MenuItem("Cylinder")) {
-                GameObject* cylinder = PrimitiveGenerator::CreateCylinder();
-                activeScene->CreateGameObject(cylinder->GetName().c_str(), cylinder);
-            }
-            if (ImGui::MenuItem("Plane")) {
-                GameObject* plane = PrimitiveGenerator::CreatePlane();
-                activeScene->CreateGameObject(plane->GetName().c_str(), plane);
-            }
-            if (ImGui::MenuItem("Cone")) {
-                GameObject* cone = PrimitiveGenerator::CreateCone();
-                activeScene->CreateGameObject(cone->GetName().c_str(), cone);
-            }
+            //if (ImGui::MenuItem("Cylinder")) {
+            //    GameObject* cylinder = PrimitiveGenerator::CreateCylinder();
+            //    activeScene->CreateGameObject(cylinder->GetName().c_str(), cylinder);
+            //}
+            //if (ImGui::MenuItem("Plane")) {
+            //    GameObject* plane = PrimitiveGenerator::CreatePlane();
+            //    activeScene->CreateGameObject(plane->GetName().c_str(), plane);
+            //}
+            //if (ImGui::MenuItem("Cone")) {
+            //    GameObject* cone = PrimitiveGenerator::CreateCone();
+            //    activeScene->CreateGameObject(cone->GetName().c_str(), cone);
+            //}
             ImGui::EndMenu();
         }
     }
